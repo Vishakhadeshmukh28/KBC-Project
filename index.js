0a5f17b092e74e2cb93e955896e020c5164f4800
vishakha deshmukh <deshmukhvishakha87@gmail.com>
2025-03-18 20:16:17 +0530

    started making quiz area page

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const welcomeScreen = document.getElementById("welcomeScreen");
+const quizArea = document.getElementById("quizArea");
+
 function startGame() {
     const username = document.getElementById("username").value;
     const error = document.getElementById("usernameError");
@@ -6,4 +9,7 @@ function startGame() {
         error.innerHTML = "Please enter username";
         return;
     }
+    error.innerHTML="";
+    welcomeScreen.classList.add("hide"); 
+    quizArea.classList.remove("hide");
 }    
